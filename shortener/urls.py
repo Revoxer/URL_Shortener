c71f175e8a65d7907fa74de5a8fafe9ff3de4dfb
Andrzej <128097055+Revoxer@users.noreply.github.com>
2025-02-24 11:05:37 +0100

    Base of App (#1)
    * base of app
    
    * little fix

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+
+from .views import IndexView, redirect_url
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path("", IndexView.as_view(), name="index"),
+    path("<str:short_code>", redirect_url, name="redirect_url"),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+import secrets
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import URL
+
+
 class IndexView(TemplateView):
-    template_name = 'shortener/index.html'
+    template_name = "shortener/index.html"
+
+    def post(self: "IndexView", request: HttpRequest) -> HttpResponse:
+        original_url = request.POST.get("url")
+        if original_url:
+            short_code = self.generate_short_code()
+            URL.objects.create(original_url=original_url, short_code=short_code)
+            short_url = request.build_absolute_uri(reverse("redirect_url", args=[short_code]))
+            return render(request, self.template_name, {"short_url": short_url})
+        return render(request, self.template_name)
+
+    @staticmethod
+    def generate_short_code() -> str:
+        while True:
+            code = secrets.token_urlsafe(6)[:8]
+            if not URL.objects.filter(short_code=code).exists():
+                return code
+
+
+def redirect_url(_request: HttpRequest, short_code: str) -> HttpResponse:
+    url = URL.objects.get(short_code=short_code)
+    url.click_count += 1
+    url.save()
+    return redirect(url.original_url)
