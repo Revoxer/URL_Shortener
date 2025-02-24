f15dfe5355cc5b6e1de6bb1cc95d74061a94c44e
revoxer <revoxers@gmail.com>
2025-02-24 10:42:14 +0100

    base of app

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class URL(models.Model):
+    original_url = models.URLField(max_length=200)
+    short_code = models.CharField(max_length=8, unique=True)
+    click_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self: object) -> str:
+        return self.original_url
